a85b608748f44fe53b65aac91f0da11c0990470e
edsilv <e.silverton@gmail.com>
2016-11-23 16:00:40 +0000

    include deps

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,16 @@ const tasks = require('gulp-tasks');
 tasks.init({
     metadata: metadata,
     // libs that MUST be included in a consuming app for this component to work
-    libs: [],
+    libs: [
+        'node_modules/base-component/dist/base-component.bundle.js'
+    ],
     // libs that MAY be included in a consuming app but are used here for example purposes
     examples: [],
     // ts definitions to copy to the 'typings' dir
-    typings: []
+    typings: [
+        'node_modules/base-component/dist/base-component.d.ts',
+        'node_modules/base-component/typings/corejs.d.ts',
+        'node_modules/base-component/typings/jquery.d.ts',
+        'node_modules/base-component/typings/node.d.ts'
+    ]
 });
