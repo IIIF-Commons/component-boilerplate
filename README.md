64c8dd2d4bd7aa100bc0b0db4ac896273ae75f7f
edsilv <e.silverton@gmail.com>
2016-11-23 13:15:46 +0000

    updated readme

--- README.md
+++ README.md
@@ -10,8 +10,3 @@ http://viewdir.github.io/component-boilerplate/examples/
     
     gulp examples
     
-##Notes
-
-###Recommended Best Practices
-
-- https://gist.github.com/edsilv/83221d47fc262230d04fbcdc32dc1d98
--- dist/component-boilerplate.d.ts
+++ dist/component-boilerplate.d.ts
@@ -1,4 +1,4 @@
-// component-boilerplate v1.0.3 https://github.com/viewdir/component-boilerplate#readme
+// component-boilerplate v1.0.4 https://github.com/viewdir/component-boilerplate#readme
 declare namespace MyComponents {
     class ExampleComponent extends _Components.BaseComponent {
         constructor(options: IExampleComponentOptions);
